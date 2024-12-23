a2f2fdbc8558b7a6ec76d29ad57f70e33096b05e
Khizer Hussain <khizwaseem@gmail.com>
2024-12-23 19:47:05 +0500

    start pattern-work

--- main.cpp
+++ main.cpp
@@ -2,7 +2,53 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    cout << "Good bye sir" << endl;
+    int n;
+    cout << "Enter the numer of rows: ";
+    cin >> n;
+
+    // int i = 1;
+
+    // while (i <= n) {
+    //     int j = 1;
+
+    //     while (j <= n) {
+    //         cout << "*";
+    //         j += 1;
+    //     }
+    //     cout << endl;
+
+    //     i += 1;
+    // }
+
+    // int i = 1;
+
+    // while (i <= n) {
+    //     int j = 1;
+
+    //     while (j <= n) {
+    //         cout << i;
+    //         j += 1;
+    //     }
+    //     cout << endl;
+
+    //     i += 1;
+    // }
+
+     int i = 1;
+
+    while (i <= n) {
+        int j = 1;
+
+        while (j <= n) {
+            // cout << j;
+            cout << n - j + 1;
+            j += 1;
+        }
+        cout << endl;
+
+        i += 1;
+    }
+
+
     return 0;
 }
